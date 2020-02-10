2c2af81c9fe975d8dcad7a2ad1bce740854eda6d
Caner Sezgin <ccanersezgin@gmail.com>
2020-02-10 19:20:30 +0300

    add form element to form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
 <template>
 
   <div>
-         <grid-layout
+
+    <v-row>
+      <v-col cols="8">
+                 <grid-layout
             :layout.sync="layout"
             :col-num="12"
             :row-height="30"
@@ -31,12 +34,71 @@
 
         </grid-item>
     </grid-layout>
+      </v-col>
+
+
+      <v-col class="px-5" cols="4">
+        {{form}}
+        
+        <v-card dark class="pa-5">
+
+          <v-select
+            v-model="selectedElement"
+            :items="items"
+            item-text="label"
+            item-value="key"
+            label="Form Element"
+            outlined
+          ></v-select>
+
+          <div v-if="selectedElement">
+            <v-row>
+              <v-col cols="12">
+                <h3>Additional Properties</h3>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                v-model="formElement.label"
+                label="Label"
+                required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="6">
+              <v-text-field
+                v-model="formElement.key"
+                label="Form Key"
+                required
+              ></v-text-field>
+              </v-col>
+            </v-row>
+
+          </div>
+
+        </v-card>
+
+        <v-card dark class="mt-3 pa-5" v-if="selectedElement">
+          <v-card-title>Preview of The Form Element</v-card-title>
+          <v-text-field
+            :label="formElement.label"
+          ></v-text-field>
+        </v-card>
+
+
+
+
+
+      </v-col>
+    </v-row>
+
+
+
 
 
     <v-btn @click="saved=!saved">
       <div v-if="saved">Edit My Form</div>
       <div v-else>Save My Form</div>
     </v-btn>
+
 
   </div>
     
@@ -53,16 +115,20 @@ import VueGridLayout from 'vue-grid-layout';
     },
     data: () => ({
       saved: false,
-      form: {
-
-      },
+      form: {},
+      formElement: {},
       layout: [
         {"x":0,"y":0,"w":2,"h":2,"i":"0", key: "name"},
         {"x":2,"y":0,"w":2,"h":4,"i":"1", key: "surname"},
         {"x":4,"y":0,"w":2,"h":5,"i":"2", key: "email"},
         {"x":6,"y":0,"w":2,"h":3,"i":"3", key: "age"},
         {"x":8,"y":0,"w":2,"h":3,"i":"4"}
-	    ]
+      ],
+      
+      selectedElement: null,
+      items: [
+        {label: 'Text Field', key: 'v-text-field'}
+      ]
     }),
   }
 </script>
