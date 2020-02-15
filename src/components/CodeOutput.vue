c178036f8d0423dd3753d5b2be88ab5632ef03f5
Caner Sezgin <ccanersezgin@gmail.com>
2020-02-15 03:06:09 +0300

    code output position done

--- src/components/CodeOutput.vue
+++ src/components/CodeOutput.vue
@@ -1,54 +1,42 @@
 <template>
-    <!--     <div>
-        <v-card class="px-4">
-            <v-row no-gutters>
-                <v-col class="pa-2" cols="4">
-                    <v-card color="red" class="px-5" height="70">x</v-card>
-                </v-col>
-                <v-col class="pa-2" cols="5" offset="2" >
-                    <v-card color="red" class="px-5" height="110">x</v-card>
-                </v-col>
-                <v-col class="pa-2" cols="7">
-                    <v-card color="red" class="px-5" height="70">x</v-card>
-                </v-col>
-                <v-col class="pa-2" cols="9">
-                    <v-card color="red" class="px-5" height="150">x</v-card>
-                </v-col>
-            </v-row>
-        </v-card>
-    </div> -->
     <div>
-        <v-card class="px-4">
-            <v-row no-gutters>
-                <v-col
+        <div>
+            <v-card class="px-4 container" :style="{gridTemplateRows: `repeat(${lastRow}, 40px [row-start])`}">
+                <div
                     v-for="item in layout"
                     :key="item.i"
-                    class="pa-2"
-                    :cols="item.w"
+                    class="item-a ma-1 px-5"
+                    :style="{
+                        gridArea: `${item.y ? item.y : 1} / ${
+                            item.x ? item.x : 1
+                        } / ${item.y + item.h} / ${item.x + item.w}`
+                    }"
                 >
-                    <v-card color="red" class="px-5" :height="item.h * 40 - 10"
-                        >x</v-card
-                    >
-                </v-col>
-            </v-row>
-        </v-card>
+                    {{ item.x }}, {{ item.y }} --> w: {{ item.w }}, h:
+                    {{ item.h }}
+                </div>
+            </v-card>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    created() {
+        this.lastRow = this.getLastRow()
+    },
     data() {
         return {
+            lastRow: 0,
             layout: [
                 {
                     x: 0,
-                    w: 4,
-                    h: 2,
+                    w: 8,
+                    h: 4,
                     minH: 1,
                     maxH: 12,
                     element: {
-                        type: "h2",
-                        desc: "dsffsddsf",
+                        type: "p",
                         category: "static",
                         component: "SimpleText"
                     },
@@ -57,57 +45,51 @@ export default {
                     moved: false
                 },
                 {
-                    x: 6,
-                    w: 5,
-                    h: 3,
-                    minH: 1,
-                    maxH: 12,
-                    element: {
-                        type: "h5",
-                        desc: "sdfds fdssfsdfdf dsfdsffds dsfdsfsd",
-                        category: "static",
-                        component: "SimpleText"
-                    },
-                    i: 1,
-                    y: 1,
-                    moved: false
-                },
-                {
-                    x: 0,
-                    w: 7,
+                    x: 4,
+                    w: 8,
                     h: 2,
                     minH: 2,
                     maxH: 2,
-                    element: {
-                        label: "sdfdsf",
-                        key: "dsfsdf",
-                        category: "input",
-                        component: "VTextField"
-                    },
-                    i: 2,
-                    y: 8,
+                    element: { category: "input", component: "VTextField" },
+                    i: 1,
+                    y: 5,
                     moved: false
                 },
                 {
-                    x: 1,
-                    w: 9,
-                    h: 4,
+                    x: 6,
+                    w: 4,
+                    h: 1,
                     minH: 1,
                     maxH: 12,
                     element: {
-                        type: "h4",
-                        desc: "qqqqqwd sadsfsdf",
+                        type: "p",
+                        desc: "tuyıuytıyutıuytı utıuytıtu",
                         category: "static",
                         component: "SimpleText"
                     },
-                    i: 3,
+                    i: 2,
                     y: 4,
                     moved: false
                 },
                 {
-                    x: 5,
-                    w: 4,
-                    h: 1,
+                    x: 0,
+                    w: 12,
+                    h: 6,
+                    minH: 1,
+                    maxH: 12,
+                    element: {
+                        type: "p",
+                        category: "static",
+                        component: "SimpleText"
+                    },
+                    i: 3,
+                    y: 7,
+                    moved: false
+                },
+                {
+                    x: 2,
+                    w: 7,
+                    h: 7,
                     minH: 1,
                     maxH: 12,
                     element: {
@@ -116,13 +98,48 @@ export default {
                         component: "SimpleText"
                     },
                     i: 4,
-                    y: 0,
+                    y: 13,
+                    moved: false
+                },
+                {
+                    x: 0,
+                    w: 12,
+                    h: 10,
+                    minH: 1,
+                    maxH: 12,
+                    element: {
+                        type: "p",
+                        category: "static",
+                        component: "SimpleText"
+                    },
+                    i: 5,
+                    y: 20,
                     moved: false
                 }
-            ].sort((a, b) => a.y - b.y)
+            ]
         };
-    }
+    },
+    methods: {
+        getLastRow() {
+            let lastRow = 0
+            this.layout.forEach(item => {
+                if(item.y + item.h > lastRow){
+                    lastRow = item.y + item.h
+                }
+            });
+            return lastRow
+        }
+    },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css">
+.container {
+    display: inline-grid;
+    grid-template-columns: repeat(12, 8.333333% [col-start]);
+}
+
+.item-a {
+    background: red;
+}
+</style>
