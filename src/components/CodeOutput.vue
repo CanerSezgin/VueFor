d37b956f52e44212c51cd0e3f6c5acda928423af
Caner Sezgin <ccanersezgin@gmail.com>
2020-02-20 19:22:06 +0300

    code output response done

--- src/components/CodeOutput.vue
+++ src/components/CodeOutput.vue
@@ -1,125 +1,72 @@
 <template>
     <div>
-        <div>
-            <v-card class="px-4 container" :style="{gridTemplateRows: `repeat(${lastRow}, 40px [row-start])`}">
-                <div
-                    v-for="item in layout"
-                    :key="item.i"
-                    class="item-a ma-1 px-5"
-                    :style="{
-                        gridArea: `${item.y ? item.y : 1} / ${
-                            item.x ? item.x : 1
-                        } / ${item.y + item.h} / ${item.x + item.w}`
-                    }"
-                >
-                    {{ item.x }}, {{ item.y }} --> w: {{ item.w }}, h:
-                    {{ item.h }}
-                </div>
-            </v-card>
-        </div>
+        {{preview}}
     </div>
 </template>
 
 <script>
+
+const getElementStr = item => {
+    let FormElementStr;
+    const element = item.element
+
+    switch (element.component) {
+        case 'VTextField':
+            FormElementStr = `
+            <v-text-field
+                v-model="form['${element.key}']"
+                label="${element.label}"
+            ></v-text-field>
+            `
+            break;
+
+        case 'SimpleText': 
+            FormElementStr = `
+            <${element.type}>${element.desc}</${element.type}>
+            `
+            break;
+
+        default:
+            break;
+    }
+
+    return `<div class="item-a ma-1 px-5" :style="{ gridArea: '${item.y+1} / ${item.x+1} / ${item.y+1+item.h} / ${item.x+1+item.w}'}">
+    ${FormElementStr}
+    </div>
+    `
+}
+
 export default {
     created() {
-        this.lastRow = this.getLastRow()
+        this.showForm();
     },
     data() {
         return {
-            lastRow: 0,
-            layout: [
-                {
-                    x: 0,
-                    w: 8,
-                    h: 4,
-                    minH: 1,
-                    maxH: 12,
-                    element: {
-                        type: "p",
-                        category: "static",
-                        component: "SimpleText"
-                    },
-                    i: 0,
-                    y: 0,
-                    moved: false
-                },
-                {
-                    x: 4,
-                    w: 8,
-                    h: 2,
-                    minH: 2,
-                    maxH: 2,
-                    element: { category: "input", component: "VTextField" },
-                    i: 1,
-                    y: 5,
-                    moved: false
-                },
-                {
-                    x: 6,
-                    w: 4,
-                    h: 1,
-                    minH: 1,
-                    maxH: 12,
-                    element: {
-                        type: "p",
-                        desc: "tuyıuytıyutıuytı utıuytıtu",
-                        category: "static",
-                        component: "SimpleText"
-                    },
-                    i: 2,
-                    y: 4,
-                    moved: false
-                },
-                {
-                    x: 0,
-                    w: 12,
-                    h: 6,
-                    minH: 1,
-                    maxH: 12,
-                    element: {
-                        type: "p",
-                        category: "static",
-                        component: "SimpleText"
-                    },
-                    i: 3,
-                    y: 7,
-                    moved: false
-                },
-                {
-                    x: 2,
-                    w: 7,
-                    h: 7,
-                    minH: 1,
-                    maxH: 12,
-                    element: {
-                        type: "p",
-                        category: "static",
-                        component: "SimpleText"
-                    },
-                    i: 4,
-                    y: 13,
-                    moved: false
-                },
-                {
-                    x: 0,
-                    w: 12,
-                    h: 10,
-                    minH: 1,
-                    maxH: 12,
-                    element: {
-                        type: "p",
-                        category: "static",
-                        component: "SimpleText"
-                    },
-                    i: 5,
-                    y: 20,
-                    moved: false
-                }
-            ]
+            preview: "",
+            layout:  [ { "x": 4, "w": 6, "h": 2, "minH": 2, "maxH": 2, "element": { "label": "fgfdg", "key": "dfgdfg", "category": "input", "component": "VTextField" }, "i": 0, "y": 2, "moved": false }, { "x": 0, "w": 8, "h": 1, "minH": 1, "maxH": 12, "element": { "type": "h2", "desc": "fdsgfds fdsgfdsgfd fdsgsfdg", "category": "static", "component": "SimpleText" }, "i": 1, "y": 4, "moved": false }, { "x": 0, "w": 12, "h": 2, "minH": 2, "maxH": 2, "element": { "label": "eewrwer", "key": "xcvxcv", "category": "input", "component": "VTextField" }, "i": 2, "y": 0, "moved": false }, { "x": 1, "w": 6, "h": 5, "minH": 1, "maxH": 12, "element": { "type": "h6", "desc": "dsfgfdg", "category": "static", "component": "SimpleText" }, "i": 3, "y": 5, "moved": false } ],
+            form: {}
         };
     },
     methods: {
+        showForm() {
+            this.preview = this.getTemplate()
+        },
+        getTemplate() {
+            const lastRow = this.getLastRow()
+            let response = ""; 
+            this.layout.forEach(item => {
+                response += getElementStr(item)
+            });
+
+            return `<v-card class="px-4 container" :style="{ 
+    gridTemplateRows: 'repeat(${lastRow}, 40px [row-start])',
+    display: 'inline-grid',
+    gridTemplateColumns: 'repeat(12, 8.333333% [col-start])'
+}">
+    ${response}
+</v-card>
+`
+        },
         getLastRow() {
             let lastRow = 0
             this.layout.forEach(item => {
@@ -132,14 +79,3 @@ export default {
     },
 };
 </script>
-
-<style lang="css">
-.container {
-    display: inline-grid;
-    grid-template-columns: repeat(12, 8.333333% [col-start]);
-}
-
-.item-a {
-    background: red;
-}
-</style>
